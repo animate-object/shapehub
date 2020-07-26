075c21f88860fcc1e51292c4f740dc4d6bdeee2a
animate-object <alex@physera.com>
2020-07-26 00:08:30 -0500

    Updates

---
+++ precache-manifest.3d51f2fa412b6732123adbd2d4914a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afcba2f943aad7f4328d9bf1b35cd03",
+    "url": "/shapehub/index.html"
+  },
+  {
+    "revision": "9113db8a1eded75357e9",
+    "url": "/shapehub/static/css/main.8c51ec13.chunk.css"
+  },
+  {
+    "revision": "2b103673c0c051ef1ddc",
+    "url": "/shapehub/static/js/2.b425bd6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shapehub/static/js/2.b425bd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9113db8a1eded75357e9",
+    "url": "/shapehub/static/js/main.1ed5e63e.chunk.js"
+  },
+  {
+    "revision": "ddb9419f3bcdd8378469",
+    "url": "/shapehub/static/js/runtime-main.328dac62.js"
+  }
+]);
